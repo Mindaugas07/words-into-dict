8281bd0066495594bd135114c473113e0b2ce65e
Mindaugas Minelga <minelga.m@gmail.com>
2023-10-10 20:57:39 +0300

    add word program

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello!")
+
+letter_dict = {}
+word_list = input("Please enter five words separated by comma: ").split(",")
+for word in word_list:
+    for letter in word:
+        if letter in letter_dict:
+           letter_dict[letter] += 1
+        else:
+           letter_dict[letter] = 1
+
+
+
+
+print(word_list)
+
+print(dict(sorted(letter_dict.items())))
